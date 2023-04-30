118d1d0c836503a3500b60a667c55bdcd1d9a673
Andrii Valenko <andreyvalenko@gmail.com>
2023-04-30 13:15:09 +0300

    rds

--- services/router.js
+++ services/router.js
@@ -1,34 +1,71 @@
-import { createStackNavigator } from "@react-navigation/stack";
+// import { createStackNavigator } from "@react-navigation/stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import RegistrationScreen from "../screens/auth/RegistrationScreen";
 import LoginScreen from "../screens/auth/LoginScreen";
 import HomeScreen from "../screens/main/HomeScreen";
+import CreatePostsScreen from "../screens/main/CreatePostsScreen";
+import CommentsScreen from "../screens/main/CommentsScreen";
 
-const MainStack = createStackNavigator();
+// const MainStack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 export const router = (isLoged) => {
-  if (!isLoged) {
-    return (
-      <MainStack.Navigator initialRouteName="Registration">
-        <MainStack.Screen
-          name="Registration"
-          component={RegistrationScreen}
-          options={{ headerShown: false }}
-        />
-        <MainStack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-      </MainStack.Navigator>
-    );
-  } else {
-    return (
-      <MainStack.Navigator initialRouteName="Home">
-        <MainStack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ headerShown: false }}
-        />
-      </MainStack.Navigator>
-    );
-  }
+  return (
+    <Stack.Navigator>
+      {isLoged ? (
+        <Stack.Group>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="CreatePosts" component={CreatePostsScreen} />
+          <Stack.Screen
+            name="Comments"
+            component={CommentsScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Group>
+      ) : (
+        <Stack.Group>
+          <Stack.Screen
+            name="Registration"
+            component={RegistrationScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Group>
+      )}
+    </Stack.Navigator>
+  );
+
+  // if (!isLoged) {
+  //   return (
+  //     <MainStack.Navigator initialRouteName="Registration">
+  //       <MainStack.Screen
+  //         name="Registration"
+  //         component={RegistrationScreen}
+  //         options={{ headerShown: false }}
+  //       />
+  //       <MainStack.Screen
+  //         name="Login"
+  //         component={LoginScreen}
+  //         options={{ headerShown: false }}
+  //       />
+  //     </MainStack.Navigator>
+  //   );
+  // } else {
+  //   return (
+  //     <MainStack.Navigator initialRouteName="Home">
+  //       <MainStack.Screen
+  //         name="Home"
+  //         component={HomeScreen}
+  //         options={{ headerShown: false }}
+  //       />
+  //     </MainStack.Navigator>
+  //   );
+  // }
 };
