be26e749b6810589d64cb5a9a32f3be68238a86c
Andrii Valenko <andreyvalenko@gmail.com>
2023-04-30 12:15:33 +0300

    rds

--- components/LogOutButton.js
+++ components/LogOutButton.js
@@ -2,12 +2,19 @@ import { user } from "../services/context";
 
 import { TouchableOpacity } from "react-native";
 import { LogOutIcon } from "./LogOutIcon";
+import { useNavigation } from "@react-navigation/native";
 
 export default LogOutButton = () => {
+  const navigation = useNavigation();
   const { setIsLoged } = user();
 
   return (
-    <TouchableOpacity onPress={() => setIsLoged(false)}>
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate("Login");
+        // setIsLoged(false);
+      }}
+    >
       <LogOutIcon />
     </TouchableOpacity>
   );
--- screens/main/homeScreen.js
+++ screens/main/homeScreen.js
@@ -1,6 +1,9 @@
 import { Text, StyleSheet } from "react-native";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { AntDesign } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+
 import LogOutButton from "../../components/LogOutButton";
 import CreatePostsScreen from "./CreatePostsScreen";
 import CommentsScreen from "./CommentsScreen";
@@ -10,7 +13,10 @@ const Tab = createBottomTabNavigator();
 
 export default HomeScreen = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      tabBarOptions={{ showLabel: false }}
+      screenOptions={{ tabBarIconStyle: { height: 40, marginTop: 9 } }}
+    >
       <Tab.Screen
         name="Posts"
         component={PostsScreen}
@@ -19,19 +25,57 @@ export default HomeScreen = () => {
           headerRightContainerStyle: { paddingRight: 16 },
           headerRight: () => <LogOutButton />,
           headerTitleContainerStyle: { paddingLeft: 16 },
+          tabBarIcon: ({ _, size }) => (
+            <Ionicons name="ios-grid-outline" size={size} color="#212121cc" />
+          ),
         }}
       />
       <Tab.Screen
         name="CreatePosts"
         component={CreatePostsScreen}
-        options={{
+        options={({ navigation }) => ({
           title: "Создать публикацию",
-        }}
+          headerLeft: () => (
+            <AntDesign
+              name="arrowleft"
+              size={24}
+              color="#212121cc"
+              onPress={() => navigation.goBack()}
+            />
+          ),
+          headerLeftContainerStyle: { paddingLeft: 16 },
+          headerTitleContainerStyle: { paddingRight: 16 },
+          tabBarIconStyle: {
+            backgroundColor: "#FF6C00",
+            marginTop: 9,
+            width: 70,
+
+            borderRadius: 20,
+          },
+          tabBarIcon: ({ _, size }) => (
+            <AntDesign name="plus" size={size} color="#fff" />
+          ),
+        })}
       />
       <Tab.Screen
         name="Comments"
         component={CommentsScreen}
-        options={{ title: "Комментарии" }}
+        options={({ navigation }) => ({
+          title: "Комментарии",
+          headerLeft: () => (
+            <AntDesign
+              name="arrowleft"
+              size={24}
+              color="#212121cc"
+              onPress={() => navigation.goBack()}
+            />
+          ),
+          headerLeftContainerStyle: { paddingLeft: 16 },
+          headerTitleContainerStyle: { paddingRight: 16 },
+          tabBarIcon: ({ _, size }) => (
+            <AntDesign name="user" size={size} color="#212121cc" />
+          ),
+        })}
       />
     </Tab.Navigator>
   );
